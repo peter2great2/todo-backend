71a82e3514b572a83bc08a55834c37a8e310a373
Peter Sambo <peter2great2@gmail.com>
2026-01-04 10:54:21 +0100

    Add new Vercel frontend URL to CORS whitelist
    Enables cross-origin requests from the latest Vercel deployment
    to allow the frontend to communicate with the backend API.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
       "http://localhost:5173",
       "https://todo-frontend-git-main-peter2great2s-projects.vercel.app",
       "https://todo-frontend-lkm4hgbwb-peter2great2s-projects.vercel.app",
+      "https://todo-frontend-steel-ten.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
