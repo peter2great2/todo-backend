3031168055768727b0a280e3eb808ed60b4da808
Peter Sambo <peter2great2@gmail.com>
2026-01-04 12:05:55 +0100

    Refactor database connection to use connection string and SSL configuration

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "tododb",
-  password: "peter",
-  port: 5432,
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://todo_db_y1tv_user:pbGoNmtDW0HuvJnwAwqq2ZjHBtYgVMBW@dpg-d5d3vv95pdvs73d2qaf0-a.oregon-postgres.render.com/todo_db_y1tv",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
