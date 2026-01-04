661fc36c6c2edbc1c1e3df9df4010d55e58f5a17
Peter Sambo <peter2great2@gmail.com>
2026-01-04 08:53:46 +0100

    Add new Vercel deployment URL to CORS origins
    Allows the main branch frontend deployment to access the API

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://todo-frontend-git-main-peter2great2s-projects.vercel.app",
       "https://todo-frontend-lkm4hgbwb-peter2great2s-projects.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
